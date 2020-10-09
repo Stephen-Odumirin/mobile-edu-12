070224ad5180e72daa9935d1b66f766e56ac35fd
stephen <stephenodumirin@gmail.com>
2020-10-09 14:57:12 +0100

    initial test commit

--- app/src/main/java/com/skool/LoginActivity.kt
+++ app/src/main/java/com/skool/LoginActivity.kt
@@ -2,10 +2,11 @@ package com.skool
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+//Login Activity For Students
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
     }
 }
