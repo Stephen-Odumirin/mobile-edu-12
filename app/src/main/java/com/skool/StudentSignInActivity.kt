3faa34ac78d4d29098aff913cd560d40283f64de
stephen <stephenodumirin@gmail.com>
2020-10-10 09:54:56 +0100

    student signin activity

--- app/src/main/java/com/skool/StudentSignInActivity.kt
+++ app/src/main/java/com/skool/StudentSignInActivity.kt
@@ -3,10 +3,10 @@ package com.skool
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 //Login Activity For Students
-class LoginActivity : AppCompatActivity() {
+class StudentSignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_student_signin)
 
     }
 }
